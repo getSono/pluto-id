72801bbbf1a104bde74d1561c360ac1ed0e1cd47
Sono <sono.arbeit@gmail.com>
2026-02-21 16:00:27 +0100

    feat: update background SVG and logo dimensions; remove unused social links

--- src/data/settings.ts
+++ src/data/settings.ts
@@ -1,5 +1,5 @@
 export const settings = {
-  site: 'https://cannonball.littlesticks.dev',
+  site: 'https://pluto.zone.id',
   name: 'Little Sticks',
   title: 'Cannonball by Little Sticks | A splash page template',
   description: 'This is a simple splash page template built with Astro by Little Sticks',
